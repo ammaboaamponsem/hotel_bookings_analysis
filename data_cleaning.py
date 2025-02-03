53719193efdeb3f18bbf8b67a59df0d99f73d7fd
Amma Boa-Amponsem <159907108+ammaboaamponsem@users.noreply.github.com>
2025-02-03 12:05:03 -0500

    Update data_cleaning.py

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,12 +1,15 @@
 # Load the necessary libraries
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
+
 
 # Load and display the dataset
 hotel_bookings_df = pd.read_csv('hotel_bookings.csv')
 print(hotel_bookings_df)
+
+
+# 1. Data Preprocessing/Cleaning Stage
+
 
 # Summary statistics of dataset
 print(hotel_bookings_df.describe())
@@ -17,7 +20,7 @@ print(hotel_bookings_df.dtypes)
 # Check for missing values
 print(hotel_bookings_df.isnull().sum())
 
-# Replace the missing values of categorical variables with their modes
+# Replace the missing values of numerical variables with their means
 numerical_vars = ['lead_time', 'arrival_date_year', 'arrival_date_week_number', 'arrival_date_day_of_month',
                   'stays_in_weekend_nights', 'stays_in_week_nights', 'adults', 'children', 'babies',
                   'is_repeated_guest', 'previous_cancellations', 'previous_bookings_not_canceled',
@@ -27,8 +30,8 @@ numerical_vars = ['lead_time', 'arrival_date_year', 'arrival_date_week_number', 
 for col in numerical_vars:
     hotel_bookings_df[col] =hotel_bookings_df[col].fillna(hotel_bookings_df[col].mean())
 
-# Replace the missing values of numerical variables with their means
-categorical_vars = ['arrival_date_month', 'meal', 'country', 'market_segment', 'distribution_channel',
+# Replace the missing values of categorical variables with their modes
+categorical_vars = ['hotel', 'arrival_date_month', 'meal', 'country', 'market_segment', 'distribution_channel',
                     'reserved_room_type', 'assigned_room_type', 'deposit_type', 'customer_type',
                     'reservation_status', 'reservation_status_date']
 
@@ -37,3 +40,17 @@ for col in categorical_vars:
 
 # Confirm if missing values have been replaced
 print(hotel_bookings_df.isnull().sum())
+
+
+# Check for duplicates
+duplicates = hotel_bookings_df[hotel_bookings_df.duplicated()]
+
+# Print the duplicate rows
+print("Duplicate Rows:")
+print(duplicates)
+
+# Remove duplicates from the DataFrame and overwrite the original DataFrame
+hotel_bookings_df.drop_duplicates(inplace=True)
+
+# Print the shape of the DataFrame after removing duplicates
+print("Shape of DataFrame after removing duplicates:", hotel_bookings_df.shape)
